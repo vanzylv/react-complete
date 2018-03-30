f9432f79ebf2f9d41a095b446c3846bec65b5e60
Viklass <vanzylv@gmail.com>
2018-03-30 16:20:08 +1300

    WIP

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -85,20 +85,20 @@ class App extends Component {
     }
 
 
-    let classes = [];
+    let assignedClasses = [];
 
     if(this.state.persons.length <= 2){
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
 
     if(this.state.persons.length <= 1){
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi there</h1>
-        <p className={classes.join(' ')}>This is working</p>
+        <p className={assignedClasses.join(' ')}>This is working</p>
         <button
           style={style}
           onClick={() => this.togglePersonsHandler('TRRRRRest')}>Switch name</button>
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Person.css';
+import classes from './Person.css';
 
 
 const person = (props) => {
@@ -7,7 +7,7 @@ const person = (props) => {
 
     return (
 
-        <div className="Person">
+        <div className={classes.Person}>
             <p onClick={props.click}>I am a {props.name} person, I am {props.age}</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
